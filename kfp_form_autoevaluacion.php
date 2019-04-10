f8e116971e77bf034fe71675574f995bb63a8c71
Juan Antonio Ruiz Rivas <juanan@us.es>
2019-04-10 07:36:38 +0200

    Cabecera del plugin y comentarios para insertar código

---
+++ kfp_form_autoevaluacion.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name:  KFP Formulario Autoevaluación
+ * Description:  Formulario para valorar el nivel de partida de los usuarios en distintos temas. 
+ * Utiliza el shortcode [kfp_aspirante_form] para que el formulario 
+ * aparezca en la página o el post que desees.
+ * Version:      0.1.1
+ * Author:       Pequeño Saltamontes
+ * Author URI:   https://kungfupress.com/
+ * PHP Version:  5.6
+ *
+ * @category Form
+ * @package  KFP
+ * @author   Pequeño Saltamontes <pequesalta@gmail.com>
+ * @license  GPLv3 http://www.gnu.org/licenses/gpl-3.0.txt
+ * @link     https://kungfupress.com
+ */
+ 
+ // Activación del plugin
+ 
+ // Creación del shortcode
+ 
+ // Creación del menú de administración
+ 
+ // Creación de la tabla de resultados
